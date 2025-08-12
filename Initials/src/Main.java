d66eaff7f87921f6d6c8567c6fe3d918cdca8824
ashima-codes <ashima.biju@gmail.com>
2025-08-12 10:31:41 -0700

    Add files via upload

---
+++ Initials/src/Main.java
@@ -0,0 +1,142 @@
+
+import kareltherobot.*;
+
+public class Main implements Directions{
+  public static void main(String[] args) {
+    World.setVisible(true);// allows us to see the run
+
+    // The line below creates a Robot that we will refer to as r.  
+    // Find out what the numbers and direction do!
+    Robot r = new Robot(6,10,North,100);
+		
+    // examples of commands you can invoke on a Robot
+    r.move();// move one step in the direction it is facing
+    r.turnLeft();
+    //r.putBeeper();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+	  r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+        r.move();
+	  r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+        r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+        r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+          r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move(); // move to B
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();   
+    r.putBeeper();
+    r.move(); 
+    // bottom of left of b
+    r.putBeeper();
+    r.turnLeft();
+          r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+            r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+          r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+
+
+
+  }
+}
