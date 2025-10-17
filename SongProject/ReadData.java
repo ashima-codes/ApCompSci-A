8391acda71824fae8bdeb821f67fd7c7a238c568
ashima-codes <ashima.biju@gmail.com>
2025-10-17 10:27:12 -0700

    Update ReadData.java

--- SongProject/ReadData.java
+++ SongProject/ReadData.java
@@ -8,33 +8,52 @@ public class ReadData{
     public Song[] read() {
         Song[] songs = new Song [28371];
         try{
-        int count = 1;
-        Scanner in = new Scanner(new File("songs ANSI.csv"));  
-        in.nextLine();
-        while(in.hasNext()){
-            String line = in.nextLine();
-            String artist = line.split(";")[0];
-            String title = line.split(";")[1];
-            int year = Integer.parseInt(line.split(";")[2]);
-            String genre = line.split(";")[3];
-            int length = Integer.parseInt(line.split(";")[3]);
-            double shakeability = Double.parseDouble(line.split(";")[4]);
-            double obscene = Double.parseDouble(line.split(";")[5]);
-            double dance = Double.parseDouble(line.split(";")[6]);
-            double loudness = Double.parseDouble(line.split(";")[7]);
-            String topic = line.split(";")[8];
-
-// line, artist, title, year, genre, lengh, , shakebaility, obscne, danceabilitym, lodness, topioc
-//public Song(String a, String g, String titl, String top, int y, int len, double dan,double loud, double ob, double shak) 
-            songs[count-1] = new Song (artist, genre, title, topic, year, length, dance, loudness, obscene,  shakeability);
-            System.out.println(count +" " + in.nextLine().split(";" )[0]);
-            count ++;                   
+            int count = 1;
+            Scanner in = new Scanner(new File("songs ANSI.csv"));  
+            in.nextLine();
+            while(in.hasNext()){
+                String line = in.nextLine();
+                String artist = line.split(";")[0];
+                String title = line.split(";")[1];
+                int year = Integer.parseInt(line.split(";")[2]);
+                String genre = line.split(";")[3];
+                int length = Integer.parseInt(line.split(";")[4]);
+                double shakeability = Double.parseDouble(line.split(";")[5]);
+                double obscene = Double.parseDouble(line.split(";")[6]);
+                double dance = Double.parseDouble(line.split(";")[7]);
+                double loudness = Double.parseDouble(line.split(";")[8]);
+                String topic = line.split(";")[9];
+                songs[count-1] = new Song (artist, genre, title, topic, year, length, dance, loudness, obscene,  shakeability);
+                //System.out.println(count +" " + in.nextLine().split(";" )[0]);
+                count ++;                  
         }  
-        System.out.println(songs[2].getTitle());        
+        System.out.println(count);         
         }
         catch(Exception e) {
             System.out.println(e);
         }
     return songs;
     }
+
+    public String getLongestTitle(Song[] list){
+        String title = "";
+        for (int i = 0; i < 28371; i++){
+            String newTitle = list[i].getTitle();
+            if(newTitle.length() > title.length()){
+                title = newTitle;
+            }
+        }
+        return title;
+    }
+    public double getLoudestSong(Song[] list){
+        double loud = 0.0;
+        for (int i = 0; i < 28371; i++){
+            double newLoud = list[i].getLoudness();
+            if(newLoud > loud){
+                loud = newLoud;
+            }
+        }
+        return loud;
+    }
+
 }
