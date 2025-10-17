6fac974f1c7bcd2aaa08674aa55b7e222b30a405
ashima-codes <ashima.biju@gmail.com>
2025-10-17 10:26:28 -0700

    Update Driver.java

--- SongProject/Driver.java
+++ SongProject/Driver.java
@@ -1,6 +1,9 @@
 public class Driver {
     public static void main (String [] args){
         ReadData r = new ReadData();
-        r.read();
+        Song [] list = r.read(); // prints numbers of songs
+        System.out.println(r.getLongestTitle(list)); // printing longest title
+        System.out.println(r.getLoudestSong(list)); // printing loudest song
     }
 }
+
