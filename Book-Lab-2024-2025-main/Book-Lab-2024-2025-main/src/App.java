f95530ac85d2706000533a0c599c58b4d845b31f
ashima-codes <ashima.biju@gmail.com>
2025-11-04 18:46:49 -0800

    Update App.java

--- Book-Lab-2024-2025-main/Book-Lab-2024-2025-main/src/App.java
+++ Book-Lab-2024-2025-main/Book-Lab-2024-2025-main/src/App.java
@@ -1,7 +1,10 @@
 class App {
   public static void main(String[] args) {
     Book aBook = new Book();
-    System.out.println(aBook.pigLatin("egg"));
+    //System.out.println(aBook.endPunctuation("hello!"));
+    System.out.println(aBook.endPunctuation("hello!"));
+    System.out.println(aBook.endPunctuation("What?!?"));   
+    System.out.println(aBook.endPunctuation("Allons-y"));  
     //System.out.println(aBook.translateWord(""));
   }
 }
