79509bd2405fb23c308bd1783eb6746cc7b1b1ab
ashima-codes <ashima.biju@gmail.com>
2025-11-14 20:06:56 -0800

    Update App.java

--- Book-Lab-2024-2025-main/Book-Lab-2024-2025-main/src/App.java
+++ Book-Lab-2024-2025-main/Book-Lab-2024-2025-main/src/App.java
@@ -1,11 +1,11 @@
 class App {
   public static void main(String[] args) {
     Book aBook = new Book("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
-    System.out.println(aBook.endPunctuation("hello!"));
-    System.out.println(aBook.endPunctuation("What?!?"));   
-    System.out.println(aBook.endPunctuation("Allons-y"));   
-    System.out.println(aBook.translateSentence("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School."));
-    System.out.println(aBook.translateSentence("\n")); 
+    //System.out.println(aBook.endPunctuation("hello!"));
+    //System.out.println(aBook.endPunctuation("What?!?"));   
+    //System.out.println(aBook.endPunctuation("Allons-y"));   
+    //System.out.println(aBook.translateSentence("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School.")); 
     aBook.readBook();
+    System.out.println("Word Count: " + aBook.getWordCount());
   }
 }
